ea671de070708a02d6a37b23b9a011edf4a95447
ammYou <ammyou.nf@gmail.com>
2018-06-11 23:07:00 +0900

    make comic model.

--- XKCDPractice/ComicModel.swift
+++ XKCDPractice/ComicModel.swift
@@ -8,3 +8,31 @@
 
 import Foundation
 import RxSwift
+import RxCocoa
+import RxAlamofire
+import Alamofire
+
+struct ComicService {
+    let latestURL = "https://xkcd.com/info.0.json"
+    let comicURL:(Int) -> String = {
+        "https://xkcd.com/\($0)/info.0.json"
+    }
+    
+    func comicFromJson(json: Any) -> Comic? {
+        if let dict = json as? [String: AnyObject]{
+            return Comic(dict)
+        }
+        return nil
+    }
+    
+    func getLatestComic() -> Observable<Comic?>{
+        return RxAlamofire.requestJSON(.get, latestURL)
+            .map{_, json in self.comicFromJson(json: json)}
+        
+    }
+    
+    func getComic(page: Int) -> Observable<Comic?>{
+        return  RxAlamofire.requestJSON(.get, comicURL(page))
+            .map{_, json in comicFromJson(json: json)}
+    }
+}
