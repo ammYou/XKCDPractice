53973b77f7227bcbd5ff8c10affb19e7feb7030e
ammYou <ammyou.nf@gmail.com>
2018-06-11 20:19:49 +0900

    Initial Commit

---
+++ XKCDPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XKCDPractice
+//
+//  Created by AmamiYou on 2018/06/11.
+//  Copyright © 2018年 AmamiYou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
