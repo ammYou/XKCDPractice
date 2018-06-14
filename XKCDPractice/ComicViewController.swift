5bcb6c20f33e6b2b73dd8ffc1625ca57f2e68e0d
ammYou <ammyou.nf@gmail.com>
2018-06-14 23:56:52 +0900

    change, view.

--- XKCDPractice/ComicViewController.swift
+++ XKCDPractice/ComicViewController.swift
@@ -8,7 +8,24 @@
 
 import Foundation
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ComicViewController: UIViewController {
+    @IBOutlet weak var comicView: UIImageView!
+    @IBOutlet weak var topLabel: UILabel!
+    @IBOutlet weak var downLabel: UILabel!
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
+    
+    var viewModel = ComicViewModel()
+    var dispose = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        viewModel.title.asDriver().drive(self.topLabel.rx.text).disposed(by: dispose)
+        //viewModel.imageUrl.asDriver().drive(onNext:{[weak self](url) in
+        })
+    }
     
 }
